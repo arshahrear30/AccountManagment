80d655c5aa16bfcfe0e6351b2a5f6aaa846a28a3
A R Shahrear <arshahrear30@gmail.com>
2025-10-28 03:19:51 +0600

    Create Signup.java

---
+++ Signup.java
@@ -0,0 +1,122 @@
+package com.arshahrear.accountmanagment;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Signup extends AppCompatActivity {
+
+    ImageView imageProfile;
+    TextView tvChangePhoto;
+    Button buttonSignup,buttonLogin;
+    TextInputEditText inputEmail,inputPassword,inputName;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_signup);
+
+        imageProfile=findViewById(R.id.imageProfile);
+        tvChangePhoto=findViewById(R.id.tvChangePhoto);
+        buttonSignup=findViewById(R.id.buttonSignup);
+        buttonLogin=findViewById(R.id.buttonLogin);
+        inputEmail=findViewById(R.id.inputEmail);
+        inputPassword=findViewById(R.id.inputPassword);
+        inputName=findViewById(R.id.inputName);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this,Login.class));
+                finish();
+
+            }
+        });
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email=inputEmail.getText().toString();
+                String password=inputPassword.getText().toString();
+                String name=inputName.getText().toString();
+
+                BitmapDrawable bitmapDrawable=(BitmapDrawable) imageProfile.getDrawable();
+                Bitmap bitmap=bitmapDrawable.getBitmap();
+                ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+                Bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+
+                byte[] imageBytes=outputStream.toByteArray();
+                String image= Base64.encodeToString(imageBytes,Base64.DEFAULT);
+
+                String url="http:";
+                StringRequest stringRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String s) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+
+                    }
+                }){ //--শেষ 1st bracket এর পরে post method এ data পাঠাতে পারি। Right click > Generate > Overridemethod >getParams<Map>
+
+
+                    @Nullable
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+
+                        Map myMap = new HashMap<String,String>();
+                        myMap.put("email","");
+                        myMap.put("password","");
+                        myMap.put("name","");
+                        myMap.put("image","");
+
+
+
+                        return super.getParams();
+                    }
+                };//--
+
+                RequestQueue requestQueue= Volley.newRequestQueue(Signup.this);
+                requestQueue.add(stringRequest);
+
+            }
+        });
+
+
+
+    }
+    //-----------
+}
