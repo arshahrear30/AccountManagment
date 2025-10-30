b72471deb5f07e8d2445fb42939560e6d8ceed51
A R Shahrear <arshahrear30@gmail.com>
2025-10-30 21:49:21 +0600

    3076  sequrity jonno  main activity theke text and password nicci .. ar er jonno MyMethods.java create korci
    Added image upload functionality and decryption process for signup.

--- signup.php
+++ signup.php
@@ -1,10 +1,16 @@
 //প্রথমে apps folder create করে নিবা । ঐটার ভিতর signup.php code add করবা এবং images file create করবা যেইটায় image গুলো store হবে।
-
 <?php
 $email = $_POST['email'];
 $name = $_POST['name'];
 $password = $_POST['password'];
 $image64 = $_POST['image'];
+$key = $_POST['key'];
+
+decryptData($key); //1st a kake decode kortay cai ..$key
+
+if($security_key=='arText'){
+
+//----------------------------
 
 $decodedImage = base64_decode($image64);
 $fileName = time().'_' . rand(1000, 100000) . '.jpeg';
@@ -21,9 +27,17 @@ if (file_put_contents($filePath,$decodedImage)) {
     else
         echo 'Signup Failed';
 
-} else {
+}
+//--------------------------------
 
-    echo 'Image Upload Failed... :(';
+
+}
+
+function decryptData($text, $pass) {
+
+    $decode = base64_decode($text);
+    $decrypted = openssl_decrypt($decode, 'AES-128-ECB', 'Juba#12345678901', OPENSSL_RAW_DATA);
+    return $decrypted;
 
 }
 ?>
