a85f8bb265acbaf482a88c274320ff08e2f2dea0
A R Shahrear <arshahrear30@gmail.com>
2025-10-31 23:56:26 +0600

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,14 +1,16 @@
 //প্রথমে apps folder create করে নিবা । ঐটার ভিতর signup.php code add করবা এবং images file create করবা যেইটায় image গুলো store হবে।
 <?php
 $email = $_POST['email'];
-$name = $_POST['name'];
 $password = $_POST['password'];
+$name = $_POST['name'];
 $image64 = $_POST['image'];
 $key = $_POST['key'];
 
-decryptData($key); //1st a kake decode kortay cai ..$key
+$security_key=decryptData($key); //1st a kake decode kortay cai ..$key
+$dec_email=decryptData($email);
+//$password=decryptData($password);  decrypt korlam na . jate kore aro safe hoi..
 
-if($security_key=='arText'){
+if($security_key=='arText' && strlen($dec_email)>0  && strlen($password)>0 ){ //user email password na dilay databse a data store hoibo na
 
 //----------------------------
 
@@ -19,7 +21,7 @@ $filePath = 'images/' . $fileName;
 if (file_put_contents($filePath,$decodedImage)) {
 
     $con = mysqli_connect('localhost', 'zftsszne_rahman', 'rahman722@gmail.com', 'zftsszne_my_database'); //user name user password database name 
-    $sql = "INSERT INTO mytable (email, name, password, image) VALUES ('$email', '$name', '$password', '$filePath')";//table name check koiro 100% same kina 
+    $sql = "INSERT INTO mytable (email, name, password, image) VALUES ('$dec_email', '$name', '$password', '$filePath')";//table name check koiro 100% same kina 
     $result = mysqli_query($con, $sql);
 
     if ($result)
