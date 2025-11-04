29ac1a95834f3fd3a19aca5ef779b616ec10a51e
A R Shahrear <arshahrear30@gmail.com>
2025-11-04 06:54:21 +0600

    3082

--- MyMethods.java
+++ MyMethods.java
@@ -1,6 +1,7 @@
 package com.arshahrear.accountmanagment;
 
 import android.util.Base64;
+import android.util.Log;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -16,13 +17,14 @@ public class MyMethods {
         String plainText = text;
         byte[] plaiTextBytes = plainText.getBytes( "UTF-8");
 
-        String password = "arText"; //"aB3$dEfGh1JkLmNo"; //16 bit
+        String password = "arText1234567890"; //"aB3$dEfGh1JkLmNo"; //16 bit
         byte[] passwordBytes = password.getBytes("UTF-8");
         //Encrypt
         SecretKeySpec secretKey = new SecretKeySpec(passwordBytes,  "AES");
-        Cipher cipher = Cipher.getInstance("AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         byte[] securedBytes = cipher.doFinal(plaiTextBytes);
+        Log.d("encripted_data", Base64.encodeToString(securedBytes, Base64.DEFAULT));
 
         //Encode
         String encodedString = Base64.encodeToString(securedBytes, Base64.DEFAULT);
