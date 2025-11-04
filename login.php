dad84a33687ab229f5539b8032e0cdd9cd5ed340
A R Shahrear <arshahrear30@gmail.com>
2025-11-04 06:57:51 +0600

    3082

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+$key = $_POST['key'];
+
+$decryptedKey = decryptData($key);
+$dec_email = decryptData($email);
+
+if ($decryptedKey == 'arText1234567890' && strlen($email) > 0) {
+
+    $con = mysqli_connect('localhost', 'zftsszne_rahman', 'rahman722@gmail.com', 'zftsszne_my_database');
+    $sql22 = "SELECT * FROM my_table WHERE email = '$dec_email' AND password = '$password' ";
+    $result = mysqli_query($con, $sql22);
+    $rows = mysqli_num_rows($result);
+
+    if ($rows > 0) {
+        echo 'VALID LOGIN';
+    } else {
+        echo 'Login Error';
+    }
+
+}
+
+function decryptData($text) {
+    $decode = base64_decode($text);
+    $decrypted = openssl_decrypt($decode, 'AES-128-ECB', 'arText1234567890', OPENSSL_RAW_DATA);
+    return $decrypted;
+}
+
+
+?>
