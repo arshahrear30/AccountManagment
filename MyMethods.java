0b021c5646f04d9258d6650fda3fd59adbb823dc
A R Shahrear <arshahrear30@gmail.com>
2025-11-09 22:47:29 +0600

    final

--- MyMethods.java
+++ MyMethods.java
@@ -17,11 +17,11 @@ public class MyMethods {
         String plainText = text;
         byte[] plaiTextBytes = plainText.getBytes( "UTF-8");
 
-        String password = "arText1234567890"; //"aB3$dEfGh1JkLmNo"; //16 bit
+        String password = "@rText1234567890";  //16 bit
         byte[] passwordBytes = password.getBytes("UTF-8");
         //Encrypt
         SecretKeySpec secretKey = new SecretKeySpec(passwordBytes,  "AES");
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         byte[] securedBytes = cipher.doFinal(plaiTextBytes);
         Log.d("encripted_data", Base64.encodeToString(securedBytes, Base64.DEFAULT));
